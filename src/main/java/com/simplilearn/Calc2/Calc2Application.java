a63e956f71f317473149c27401359028f4ae40be
student <amiksha.thakkar@gmail.com>
2021-08-02 17:11:28 +0000

    logic and test

---
+++ src/main/java/com/simplilearn/Calc2/Calc2Application.java
@@ -0,0 +1,13 @@
+package com.simplilearn.Calc2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Calc2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Calc2Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/simplilearn/Calc2/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.simplilearn.Calc2;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Calc2Application.class);
+	}
+
+}
